36b5f747a8aa3a6e03e4a7d3ddf044aec183b86c
Khantminaung313 <khantminaung313@gmail.com>
2024-09-03 11:40:30 +0630

    interactive add-to-cart

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -45,7 +45,9 @@ products.forEach((product) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary" data-product-id="${
+				product.id
+			}">
             Add to Cart
           </button>
         </div>
@@ -53,3 +55,27 @@ products.forEach((product) => {
 	productData += html;
 });
 document.querySelector(".products-grid").innerHTML = productData;
+const addToCartButtons = document.querySelectorAll(".add-to-cart-button");
+let cartQuantity = 0;
+addToCartButtons.forEach((button) => {
+	button.addEventListener("click", () => {
+		let productId = button.dataset.productId;
+		let matchingItem;
+		cart.forEach((item) => {
+			if (item.productId === productId) {
+				matchingItem = item;
+			}
+		});
+		if (matchingItem) {
+			matchingItem.quantity += 1;
+		} else {
+			cart.push({
+				productId: productId,
+				quantity: 1,
+			});
+		}
+		cartQuantity += 1;
+        document.querySelector(".cart-quantity").innerHTML = cartQuantity;
+        
+	});
+});
